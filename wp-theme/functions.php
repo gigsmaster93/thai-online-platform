fa3b60ae61eec7269b2144a1965d61253793e9fb
gigsmaster93 <gigsmaster93@gmail.com>
2026-09-18 14:46:02 +1000

    Bust cache for product parity CSS

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-define('THAI_ONLINE_THEME_VERSION', '4.4.5');
+define('THAI_ONLINE_THEME_VERSION', '4.4.6');
 require_once get_template_directory() . '/inc/parity.php';
 
 add_action('after_setup_theme', function () {
