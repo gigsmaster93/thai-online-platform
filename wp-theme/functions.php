437ec9d641567da3936c50343590fd1495d2ed6e
gigsmaster93 <gigsmaster93@gmail.com>
2026-09-18 21:19:02 +1000

    Bump frontend assets to 4.4.16

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-define('THAI_ONLINE_THEME_VERSION', '4.4.15');
+define('THAI_ONLINE_THEME_VERSION', '4.4.16');
 require_once get_template_directory() . '/inc/parity.php';
 
 add_action('after_setup_theme', function () {
