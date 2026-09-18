245a81a2bd0bc2e731a73b06114291e89ae97ec2
gigsmaster93 <gigsmaster93@gmail.com>
2026-09-18 14:09:35 +1000

    Match legacy homepage product selections

--- wp-theme/front-page.php
+++ wp-theme/front-page.php
@@ -1,21 +1,31 @@
 <?php
 get_header();
 
-function thai_home_cards($args, $prefix) {
-    $q = new WP_Query(array_merge([
-        'post_type' => 'thai_excursion',
-        'post_status' => 'publish',
-        'posts_per_page' => 7,
-        'no_found_rows' => true,
-    ], $args));
+function thai_home_cards_by_ucoz(array $ucoz_ids, string $prefix): void {
     echo '<div class="shop-main-list"><div class="goods-list with-clear">';
-    while ($q->have_posts()) {
-        $q->the_post();
-        thai_render_excursion_card(get_the_ID(), $prefix);
+
+    foreach ($ucoz_ids as $ucoz_id) {
+        $posts = get_posts([
+            'post_type'      => 'thai_excursion',
+            'post_status'    => 'publish',
+            'meta_key'       => '_ucoz_shop_id',
+            'meta_value'     => (int) $ucoz_id,
+            'posts_per_page' => 1,
+            'fields'         => 'ids',
+        ]);
+
+        if ($posts) {
+            thai_render_excursion_card((int) $posts[0], $prefix);
+        }
     }
+
     echo '</div></div>';
-    wp_reset_postdata();
 }
+
+$home_fresh = [87, 47, 24, 33, 55, 56, 86];
+$home_viewed = [496, 47, 480, 172, 137, 86, 24, 89, 82];
+$home_popular = [93, 497, 495, 505, 492, 485, 504, 496, 491];
+$home_free = [470, 267, 266, 258, 257, 256, 255];
 ?>
 <br>
 <h1 style="text-align:center;">Экскурсии в Паттайе - Паттайя экскурсии <?php echo esc_html(wp_date('Y')); ?></h1>
@@ -27,10 +37,10 @@ function thai_home_cards($args, $prefix) {
     <li><a href="#tab-3">Популярные</a></li>
     <li><a href="#tab-4">Бесплатно</a></li>
   </ul>
-  <div id="tab-1" class="tab-body"><?php thai_home_cards(['orderby'=>'date','order'=>'DESC'], 'new'); ?></div>
-  <div id="tab-2" class="tab-body" style="display:none"><?php thai_home_cards(['orderby'=>'rand'], 'view'); ?></div>
-  <div id="tab-3" class="tab-body" style="display:none"><?php thai_home_cards(['orderby'=>'date','order'=>'ASC'], 'popular'); ?></div>
-  <div id="tab-4" class="tab-body" style="display:none"><?php thai_home_cards(['meta_query'=>[['key'=>'_thai_price','value'=>0,'compare'=>'=','type'=>'NUMERIC']]], 'free'); ?></div>
+  <div id="tab-1" class="tab-body"><?php thai_home_cards_by_ucoz($home_fresh, 'inf10'); ?></div>
+  <div id="tab-2" class="tab-body" style="display:none"><?php thai_home_cards_by_ucoz($home_viewed, 'top_view'); ?></div>
+  <div id="tab-3" class="tab-body" style="display:none"><?php thai_home_cards_by_ucoz($home_popular, 'top_sold'); ?></div>
+  <div id="tab-4" class="tab-body" style="display:none"><?php thai_home_cards_by_ucoz($home_free, 'free'); ?></div>
   <div class="viewAlldiv"><a class="viewAllbtn" href="<?php echo esc_url(home_url('/shop/all')); ?>">Смотреть все варианты</a></div>
 </section>
 
