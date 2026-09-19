19a3dfe9c7d75b101b70dca2407950894ab53bd7
gigsmaster93 <gigsmaster93@gmail.com>
2026-09-19 16:26:45 +1000

    Finish single photo geometry parity

--- wp-plugin/src/Community.php
+++ wp-plugin/src/Community.php
@@ -17,9 +17,9 @@ class TOP_Community {
         add_shortcode('thai_contact_form', [__CLASS__, 'contact_form']);
         add_filter('redirect_canonical', static function($url){return get_query_var('top_community') ? false : $url;});
         add_action('wp_enqueue_scripts', static function(){
-            if(get_query_var('top_community') || is_page('contact')) wp_enqueue_style('thai-community',plugins_url('assets/community.css',TOP_PLUGIN_FILE),[], '4.4.43');
+            if(get_query_var('top_community') || is_page('contact')) wp_enqueue_style('thai-community',plugins_url('assets/community.css',TOP_PLUGIN_FILE),[], '4.4.44');
             if(get_query_var('top_community')==='photo') wp_enqueue_style('thai-photo-legacy',home_url('/_st/photo.css'),[], '525');
-            if(get_query_var('top_community')==='forum') wp_enqueue_script('thai-community-forum',plugins_url('assets/community.js',TOP_PLUGIN_FILE),[], '4.4.43',true);
+            if(get_query_var('top_community')==='forum') wp_enqueue_script('thai-community-forum',plugins_url('assets/community.js',TOP_PLUGIN_FILE),[], '4.4.44',true);
             if(is_page('contact')) wp_enqueue_style('thai-contact-legacy',home_url('/css/pages/1.css'),[], '52535335');
         },1000);
     }
