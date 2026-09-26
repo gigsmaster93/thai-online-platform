275e92cd3b598634c8b751425bb71b850409204b
gigsmaster93 <gigsmaster93@gmail.com>
2026-09-26 20:47:34 +1000

    Restore legacy FAQ route and UI assets

--- wp-plugin/src/LegacyRoutes.php
+++ wp-plugin/src/LegacyRoutes.php
@@ -5,6 +5,8 @@ if (!defined('ABSPATH')) exit;
 class TOP_Legacy_Routes {
     public static function boot() {
         add_action('init', [__CLASS__, 'routes'], 40);
+        add_action('pre_get_posts', [__CLASS__, 'faq_archive_order'], 50);
+        add_action('template_redirect', [__CLASS__, 'legacy_asset'], 0);
         add_action('template_redirect', [__CLASS__, 'forum_jump'], 0);
         add_action('template_redirect', [__CLASS__, 'shop_people_jump'], 0);
         add_filter('template_include', [__CLASS__, 'missing_jump'], 200);
@@ -14,11 +16,42 @@ class TOP_Legacy_Routes {
         add_rewrite_rule('^photo/(?:.+/)?([0-9]+)-([1-9][0-9]*)/?$', 'index.php?top_community=photo&top_section=$matches[1]&top_page=$matches[2]', 'top');
         add_rewrite_tag('%top_forum_jump%', '([01])');
         add_rewrite_tag('%top_shop_people_jump%', '([01])');
+        add_rewrite_tag('%top_legacy_asset%', '([a-z0-9_]+)');
+        add_rewrite_rule('^faq/1-1/?$', 'index.php?tocms_module=faq', 'top');
+        add_rewrite_rule('^\\.s/sm/1/(smile|tongue)\\.gif$', 'index.php?top_legacy_asset=$matches[1]', 'top');
+        add_rewrite_rule('^\\.s/img/icon/(thumbu_|thumbd_)\\.png$', 'index.php?top_legacy_asset=$matches[1]', 'top');
         add_rewrite_rule('^shop/([0-9]+)/desc/people/[0-9]+/?$', 'index.php?top_shop_people_jump=1&top_id=$matches[1]', 'top');
         add_rewrite_rule('^forum/([0-9]+)-([0-9]+)-0-17(?:-1)?/?$', 'index.php?top_forum_jump=1&top_section=$matches[1]&top_id=$matches[2]', 'top');
         add_rewrite_rule('^immigration-to-thailand-2022/?$', 'index.php?top_module=legacy_static_page&top_id=92', 'top');
         add_rewrite_rule('^art_in_paradise/?$', 'index.php?top_community=photo&top_section=30', 'top');
         add_rewrite_rule('^free-transfers-shopping-pattaya/?$', 'index.php?top_community=photo&top_section=329', 'top');
+    }
+
+    public static function faq_archive_order($query) {
+        if (is_admin() || get_query_var('tocms_module') !== 'faq') return;
+        if ($query->get('post_type') !== 'thai_faq') return;
+        $query->set('orderby', ['menu_order'=>'ASC', 'date'=>'ASC', 'ID'=>'ASC']);
+        $query->set('order', 'ASC');
+    }
+
+    public static function legacy_asset() {
+        $key = (string) get_query_var('top_legacy_asset');
+        if (!$key) return;
+        $map = [
+            'smile' => ['smile.gif', 'image/gif'],
+            'tongue' => ['tongue.gif', 'image/gif'],
+            'thumbu_' => ['thumbu_.png', 'image/png'],
+            'thumbd_' => ['thumbd_.png', 'image/png'],
+        ];
+        if (!isset($map[$key])) return;
+        $file = TOP_PLUGIN_DIR . 'assets/legacy-ui/' . $map[$key][0];
+        if (!is_file($file)) return;
+        status_header(200);
+        header('Content-Type: ' . $map[$key][1]);
+        header('Content-Length: ' . filesize($file));
+        header('Cache-Control: public, max-age=31536000, immutable');
+        readfile($file);
+        exit;
     }
 
     public static function jump_target($section, $legacy_topic) {
